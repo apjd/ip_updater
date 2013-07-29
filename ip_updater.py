c4115dabdea93c056bf59880ed529699a9b67b99
Ali <ali@SA11.(none)>
2013-07-29 03:21:19 +0200

    first commit

---
+++ ip_updater.py
@@ -0,0 +1,41 @@
+import requests, sys, time
+from time import gmtime, strftime
+
+# configurations
+rec_id= '72584584'
+tkn = '8b6c3516b4013ee3cf10230fdcd6395048d54'
+email = 'ali.glider@gmail.com'
+domain = 'apjd.de'
+sub_name= 'sub'
+
+
+def get_current_ip():
+	r = requests.get('http://api.externalip.net/ip')
+	ip = r.text
+	return(ip)
+
+def update_ip(rec_id,tkn,email,domain,sub_name,new_ip):
+	params = { 'a':'rec_edit', 'tkn': tkn, 'id': rec_id, 'email': email, 'z': domain, 'type':'A', 'name':sub_name, 'content': new_ip, 'service_mode': '1', 'ttl': '1' } 
+	r = requests.get('https://www.cloudflare.com/api_json.html', params = params)
+	if r.json()['result'] == 'success':
+		return True
+	else:
+		return False
+
+while True:
+
+	current_ip = get_current_ip()
+	with open('old_ip.txt', 'r') as f:
+		old_ip = f.readline()
+
+	if old_ip == current_ip:
+		print("IP-Address not changed - " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	else:	
+		if update_ip(rec_id=rec_id, tkn=tkn, email=email, domain=domain, sub_name=sub_name, new_ip=current_ip):
+			with open('old_ip.txt', 'w') as f:
+				f.write(current_ip)
+			print("IP-Address successfully updated!" + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+		else:
+			print("A Problem occured!" + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	time.sleep(10)
+
